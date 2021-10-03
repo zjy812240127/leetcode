93840ce1348112e0b9c20b2067f69ae4a2057dc1
zjy <zjy@812240127.com>
2021-10-03 08:57:18 +0800

    2021/10/3

---
+++ src/com/cute/leetcode/editor/cn/LockSupportDemo.java
@@ -0,0 +1,39 @@
+package com.cute.leetcode.editor.cn;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * @author Joya Zou
+ * @date 2021年10月02日 13:42
+ */
+public class LockSupportDemo {
+
+    public static void main(String[] args) {
+        Thread t =new Thread(()->{
+            for(int i=0; i<20; i++){
+                if(i==5) {
+                    /** 停车 */
+                    LockSupport.park();
+                }
+                System.out.println(i);
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        t.start();
+        try {
+            TimeUnit.SECONDS.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("经过10s后执行unpark");
+        /** 开车，指定让某个线程继续执行 */
+        LockSupport.unpark(t);
+    }
+
+}
