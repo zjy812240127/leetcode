cc91029bc99e5774e255f4b044d27d49283cdfdd
zjy <zjy@812240127.com>
2021-12-11 10:31:57 +0800

    2021/12/11

--- src/com/msb/algorithm/graph/Dijkstra.java
+++ src/com/msb/algorithm/graph/Dijkstra.java
@@ -1,8 +1,6 @@
 package com.msb.algorithm.graph;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /**
  * 迪杰斯特拉算法求解起点到每一个终点的最短距离
@@ -20,12 +18,43 @@ public class Dijkstra {
         HashMap<Node, Integer> distanceMap = new HashMap<>();
         // 被锁定的已用的节点
         Set<Node> usedNodesSet = new HashSet<>();
+        // 要遍历的点
+        LinkedList<Node> path = new LinkedList<>();
 
         HashMap<Integer, Node> sourceNodes = graph.nodes;
         Node startNode = sourceNodes.get(0);
+        path.add(startNode);
+        distanceMap.put(startNode, 0);
 
+        while (path.size()>0){
+            Node node = path.removeFirst();
+            if (!usedNodesSet.contains(node)) {
+                ArrayList<Edge> edges = node.edges;
 
+                for (Edge edge : edges) {
+                    Node toNode = edge.to;
+                    path.add(toNode);
+                    if (!distanceMap.containsKey(toNode)) {
+                        // 第一次从无穷改为一个从起点可达的距离
+                        distanceMap.put(toNode, distanceMap.get(node) + edge.weight);
+                    } else {
+                        // 更新最近的距离
+                        distanceMap.put(toNode, Math.min(distanceMap.get(toNode), distanceMap.get(node) + edge.weight));
+                    }
 
+                }
+                usedNodesSet.add(node);
+            }
+        }
 
+        return distanceMap;
+    }
+
+    public static void main(String[] args) {
+        Dijkstra dijkstra = new Dijkstra();
+        HashMap<Node, Integer> distanceMap = dijkstra.getDistanceMap(GraphGenerator.getMyGraph());
+        for (Node node: distanceMap.keySet()){
+            System.out.println("到点" + node.value + "的距离为：" +distanceMap.get(node));
+        }
     }
 }
